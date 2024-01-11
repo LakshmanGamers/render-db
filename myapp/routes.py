5e6c448f2d221382d3b27407d2be6a661a33409f
LakshmanGamers <lakshmanreddy458@gmail.com>
2024-01-11 22:16:09 +0530

    converted to json

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, jsonify
 
 from .extensions import db
 from .models import User
@@ -8,12 +8,15 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     users = User.query.all()
-    users_list_html = [f"<li>{ user.username }</li>" for user in users]
-    return f"<ul>{''.join(users_list_html)}</ul>"
+    res = [user.username for user in users]
+    return jsonify(users=res)
 
 @main.route('/add/<username>')
 def add_user(username):
-    db.session.add(User(username=username))
-    db.session.commit()
-    return redirect(url_for("main.index"))
-
+    try:
+        new_user = User(username=username)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(message='User added successfully')
+    except Exception as e:
+        return jsonify(error=str(e))
